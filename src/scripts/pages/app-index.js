30f51643056f27b1d5c9fa74350700e362f5b887
Kylen8036 <18753377092@163.com>
2016-09-30 11:52:35 +0800

    v1.0.0 modify help-text

--- src/scripts/pages/app-index.js
+++ src/scripts/pages/app-index.js
@@ -1,6 +1,10 @@
 
+// test header and footer statr
 
-var header = require("../tpls/header.html");
+// var header = require("../tpls/header.html");
 // var footer = require("../tpls/footer.html");
 // $('body').prepend(footer);
-$('body').prepend(header);
+// $('body').prepend(header);
+
+// test header and footer end
+
