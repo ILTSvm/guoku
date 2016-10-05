bb762feb418d8afbd349e750e6aab6fac805e39d
wlwywlqk <wlwywlqk@gmail.com>
2016-10-05 18:18:12 +0800

    banner

--- src/scripts/utils/common.util.js
+++ src/scripts/utils/common.util.js
@@ -1,9 +1,23 @@
 var common = {
   render: function (str) {
     $('body').prepend(str);
+  },
+  tpl: function(id, data) {
+    if(data.swiper){
+      var swiperjs = '<script> new Swiper("#'+data.id+'", {'
+      for (var key in data.swiper) {
+        if(typeof data.swiper[key] === 'string'){
+          swiperjs+=key+':"'+data.swiper[key].toString()+'",';
+        }else{
+          swiperjs+=key+':'+data.swiper[key].toString()+',';
+        }
+      }
+      swiperjs += '})</script> ';
+      data.swiper = swiperjs;
+    }
+    return template(id,data);
   }
 };
-
 
 module.exports = common;
 
