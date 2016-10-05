4595a00f9c7249231531f78944b3d33e6376ad59
Kylen8036 <18753377092@163.com>
2016-10-05 17:48:14 +0800

    add anyway

--- src/scripts/pages/app-index.js
+++ src/scripts/pages/app-index.js
@@ -1,10 +1,12 @@
 
 // test header and footer statr
 
-// var header = require("../tpls/header.html");
+var header = require("../tpls/header.html");
+
+$('body').prepend(header);
+
+
 // var footer = require("../tpls/footer.html");
 // $('body').prepend(footer);
-// $('body').prepend(header);
 
 // test header and footer end
-
