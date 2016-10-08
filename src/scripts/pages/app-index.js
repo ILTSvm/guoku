cc9124feb364e43110b4725bddaf38c1e3cb8fc1
Heav3n <315331970@qq.com>
2016-10-08 08:39:54 +0800

    modefied

--- src/scripts/pages/app-index.js
+++ src/scripts/pages/app-index.js
@@ -1,10 +1,10 @@
 
 // test header and footer statr
 
-// var header = require("../tpls/header.html");
-// var footer = require("../tpls/footer.html");
-// $('body').prepend(footer);
-// $('body').prepend(header);
+   var header = require("../tpls/header.html");
+   var footer = require("../tpls/footer.html");
+   $('body').prepend(footer);
+   $('body').prepend(header); 
 
 // test header and footer end
 
